fa60c98c7328a00ba9f271ee8a0b6b86b778a37a
Jennie Long <longjen100@gmail.com>
2025-04-22 12:51:38 -0700

    added modal styling.

--- app/(tabs)/index/modal.tsx
+++ app/(tabs)/index/modal.tsx
@@ -1,20 +1,53 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 import { Link, router } from "expo-router";
+import Animated, { FadeIn, SlideInDown } from "react-native-reanimated";
+import { FilledPill } from "./../../../components/PillButton";
 
+// TODO: If using this later, add in parameter for JSX content
 export default function Modal() {
   const isPresented = router.canGoBack();
+  const childContent = (
+    <>
+      <View className="flex items-center justify-center bg-white">
+        <Text className="m-5">Modal title</Text>
+        <FilledPill
+          label="Okay"
+          callback={() => {
+            {
+              /* If the modal was added on a stack, return to
+              previous page. Otherwise, return to index */
+            }
+            router.navigate(isPresented ? "../" : "/");
+          }}></FilledPill>
+      </View>
+    </>
+  );
 
   return (
-    <View className="m-auto flex flex-1 justify-center">
-      <View className="align-center flex-shrink-1 flex size-80 justify-center rounded-xl border-2 border-black bg-white p-10">
-        <Text>Modal title</Text>
-        {/* If the modal was added on a stack, return to
-        previous page. Otherwise, return to index */}
-        <Link href={isPresented ? "../" : "/"}>
-          {isPresented ? "Dismiss" : "Return to main page"}
-        </Link>
-      </View>
-    </View>
+    <Animated.View
+      entering={FadeIn}
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#00000040",
+      }}>
+      {/* Dismiss modal when pressing outside */}
+      <Link href={"/"} asChild>
+        <Pressable style={StyleSheet.absoluteFill} />
+      </Link>
+      <Animated.View
+        entering={SlideInDown}
+        style={{
+          width: "70%",
+          height: "70%",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "white",
+        }}>
+        {childContent}
+      </Animated.View>
+    </Animated.View>
   );
 }
