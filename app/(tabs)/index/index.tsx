aa0b95e3f25aec9a33110a7f50a1a54c902a88c5
Jennie Long <longjen100@gmail.com>
2025-04-22 14:11:04 -0700

    added basic flatlist component.

--- app/(tabs)/index/index.tsx
+++ app/(tabs)/index/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "expo-router";
 import Counter from "../../../components/Counter";
 import { FilledPill, OutlinedPill } from "../../../components/PillButton";
+import List from "../../../components/List";
 
 export default function index() {
   const router = useRouter();
@@ -15,6 +16,7 @@ export default function index() {
       <Counter></Counter>
       <FilledPill label="Message" callback={callback}></FilledPill>
       <OutlinedPill label="Message" callback={callback}></OutlinedPill>
+      <List></List>
     </>
   );
 }
