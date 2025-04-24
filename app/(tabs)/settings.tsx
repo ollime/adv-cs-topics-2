c67db45fd1c83c25d64f8a4403b886d893553682
Jennie Long <longjen100@gmail.com>
2025-04-23 19:31:11 -0700

    added functionality to dark mode setting.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -5,13 +5,17 @@ import RadioSelect from "../../components/RadioSelect";
 
 export default function secondPage() {
   const { setColorScheme } = useColorScheme();
-  setColorScheme("dark");
+  function changeTheme(value: "dark" | "light" | "system") {
+    console.log(value);
+    setColorScheme(value);
+  }
 
   return (
     <View className="flex flex-1 bg-background dark:bg-backgroundDark">
       <RadioSelect
         label="Dark mode"
-        options={["dark", "light", "automatic"]}></RadioSelect>
+        options={["system", "light", "dark"]}
+        onChangeOption={changeTheme}></RadioSelect>
     </View>
   );
 }
