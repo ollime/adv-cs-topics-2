09c3e31e61932ef6ac86a42aea308b087560200b
Jennie Long <longjen100@gmail.com>
2025-04-25 22:52:19 -0700

    fixed metro.config.cjs import style.

--- metro.config.cjs
+++ metro.config.cjs
@@ -1,5 +1,5 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+import { getDefaultConfig } from "expo/metro-config";
+import { withNativeWind } from "nativewind/metro";
 
 const config = getDefaultConfig(__dirname);
 
