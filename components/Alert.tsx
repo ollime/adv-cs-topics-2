7226f63de3a7b79efa59e70653c799c68beb7305
Jennie Long <longjen100@gmail.com>
2025-04-28 12:06:24 -0700

    hiding alert by clicking on X icon

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -1,19 +1,32 @@
 /** @overview Alert popup that closes when clicked on. */
 
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function Alert({ label }: { label: string }) {
+  const [hide, setHide] = React.useState<boolean>(false);
+  const hideAlert = () => {
+    setHide(true);
+  };
+
   return (
     <>
-      <View className="m-5 mx-10 flex flex-row items-center justify-between rounded-lg border border-red-400 bg-red-100 p-4">
-        <View className="flex flex-row">
-          <Text className="font-bold text-red-500">WARNING: </Text>
-          <Text className="text-red-500">Changes not saved.</Text>
-        </View>
-        <MaterialIcons name="close" size={24} color="red" />
-      </View>
+      {!hide ? (
+        <>
+          <View className="m-5 mx-10 flex flex-row items-center justify-between rounded-lg border border-red-400 bg-red-100 p-4">
+            <View className="flex flex-row">
+              <Text className="font-bold text-red-500">WARNING: </Text>
+              <Text className="text-red-500">Changes not saved.</Text>
+            </View>
+            <Pressable onPress={hideAlert}>
+              <MaterialIcons name="close" size={24} color="red" />
+            </Pressable>
+          </View>
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 }
