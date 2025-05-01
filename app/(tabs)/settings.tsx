5df9ece47926dbc6a620b1adfdde6271f859a265
Jennie Long <longjen100@gmail.com>
2025-05-01 15:40:58 -0700

    added async storage functions for theme setting

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -2,14 +2,37 @@ import React from "react";
 import { View } from "react-native";
 import { useColorScheme } from "nativewind";
 import RadioSelect from "../../components/RadioSelect";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function secondPage() {
   const { setColorScheme } = useColorScheme();
-  function changeTheme(value: "dark" | "light" | "system" | string) {
+  async function changeTheme(value: "dark" | "light" | "system" | string) {
     if (value == "dark" || value == "light" || value == "system") {
       setColorScheme(value);
+      saveValue("theme", value);
+      const newValue = await getData("theme");
+      console.log(newValue);
     }
   }
+
+  const saveValue = async (key: string, value: string) => {
+    try {
+      await AsyncStorage.setItem(key, value);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getData = async (key: string) => {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        return value;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <View className="flex flex-1 bg-background dark:bg-backgroundDark">
