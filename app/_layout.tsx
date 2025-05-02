9ed5337a01c000b6bc802d7b01cf19b82391d3c8
Jennie Long <longjen100@gmail.com>
2025-05-02 09:44:59 -0700

    fixed bug where previously set dark theme is only applied when visiting settings page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,36 @@
 /** @overview Root layout for the app */
 
 import React from "react";
+import { useColorScheme } from "nativewind";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Stack } from "expo-router";
 
 import "./../global.css";
 
 export default function App() {
+  const { setColorScheme } = useColorScheme();
+
+  React.useEffect(() => {
+    const setThemeColor = async () => {
+      const theme = await getThemeData();
+      if (theme == "dark" || theme == "light" || theme == "system") {
+        setColorScheme(theme);
+      }
+    };
+    setThemeColor();
+  }, []);
+
+  const getThemeData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("theme");
+      if (value !== null) {
+        return value;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <>
       <Stack>
