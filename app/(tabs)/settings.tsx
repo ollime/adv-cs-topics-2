d63db670d238baa477b794eeec2dbe6a1b4bbf10
Jennie Long <longjen100@gmail.com>
2025-05-02 09:36:59 -0700

    loaded theme color settings from storage

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -5,17 +5,22 @@ import RadioSelect from "../../components/RadioSelect";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function secondPage() {
+  const [themeColor, setThemeColor] = React.useState<
+    "dark" | "light" | "system"
+  >();
   const { setColorScheme } = useColorScheme();
-  async function changeTheme(value: "dark" | "light" | "system" | string) {
-    if (value == "dark" || value == "light" || value == "system") {
-      setColorScheme(value);
-      saveValue("theme", value);
-      const newValue = await getData("theme");
-      console.log(newValue);
-    }
-  }
 
-  const saveValue = async (key: string, value: string) => {
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const theme = await getData("theme");
+      if (theme == "dark" || theme == "light" || theme == "system") {
+        changeTheme(theme);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const saveInStorage = async (key: string, value: string) => {
     try {
       await AsyncStorage.setItem(key, value);
     } catch (e) {
@@ -34,12 +39,21 @@ export default function secondPage() {
     }
   };
 
+  async function changeTheme(value: "dark" | "light" | "system" | string) {
+    if (value == "dark" || value == "light" || value == "system") {
+      setColorScheme(value);
+      setThemeColor(value);
+      saveInStorage("theme", value);
+    }
+  }
+
   return (
     <View className="flex flex-1 bg-background dark:bg-backgroundDark">
       <RadioSelect
         label="Dark mode"
         options={["system", "light", "dark"]}
-        onChangeOption={changeTheme}></RadioSelect>
+        onChangeOption={changeTheme}
+        selected={themeColor}></RadioSelect>
     </View>
   );
 }
