715cbd48e8b7e9fb75fc00c8a28de76759969618
Jennie Long <longjen100@gmail.com>
2025-05-19 13:01:52 -0700

    added dropdown component for selecting starred events

--- app/(tabs)/(star)/starEvent.tsx
+++ app/(tabs)/(star)/starEvent.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { View } from "react-native";
+import DropdownComponent from "components/inputFields/DropdownField";
 import Modal from "../../../components/Modal";
 
 export default function StarEventModal() {
   const childContent = (
     <>
-      <View>!!!</View>
+      <DropdownComponent
+        data={testData}
+        preset={["quarter", "red", "key"]}></DropdownComponent>
     </>
   );
 
@@ -15,3 +17,72 @@ export default function StarEventModal() {
     </>
   );
 }
+const testData = [
+  {
+    key: "quarter",
+    color: "purple-400",
+    type: "since",
+    description: "A description of this event.",
+    startTime: 1725623522,
+    endTime: 1749222291,
+  },
+  {
+    key: "good-bye",
+    color: "purple-400",
+    type: "until",
+    endTime: 1762660048,
+  },
+  { key: "zephyr", color: "primary", type: "since", startTime: 1715309248 },
+  {
+    key: "stream",
+    type: "elapsed",
+    color: "blue-400",
+    startTime: 1725623522,
+    endTime: 1748222240,
+  },
+  { key: "unit", type: "until" },
+  {
+    key: "receptive",
+    color: "orange-400",
+    description:
+      "This is what a long description looks like. It may break off into multiple lines.",
+  },
+  {
+    key: "summer",
+    color: "red-400",
+    type: "since",
+    description: `For multi-line descriptions,
+          use \` markers or \\n
+          newline characters.`,
+  },
+  { key: "tear", color: "green-400", type: "until", endTime: 1802660048 },
+  { key: "course", color: "orange-400" },
+  { key: "steel", color: "blue-400" },
+  { key: "red", type: "until", endTime: 1903593432 },
+  { key: "hungry", type: "until", endTime: 2003593432 },
+  {
+    key: "impartial",
+    type: "until",
+    endTime: 1749391692,
+  },
+  {
+    key: "writing",
+    color: "green-400",
+    type: "since",
+    startTime: 1697424893,
+  },
+  { key: "key" },
+  { key: "quill" },
+  { key: "tearful" },
+  { key: "swear" },
+  { key: "left" },
+  { key: "cute" },
+  { key: "declare" },
+  { key: "kick" },
+  { key: "proud" },
+  { key: "cheap" },
+  { key: "wistful" },
+  { key: "birthday" },
+  { key: "torpid" },
+  { key: "heavy" },
+];
---
+++ components/inputFields/DropdownField.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { MultiSelect } from "react-native-element-dropdown";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { ListItem } from "../../types";
+
+export default function DropdownComponent({
+  data,
+  preset,
+}: {
+  data;
+  preset: Array<string>;
+}) {
+  const [value, setValue] = React.useState(preset);
+  const [isFocus, setIsFocus] = React.useState(false);
+
+  const renderLabel = () => {
+    if (value || isFocus) {
+      return (
+        <Text style={[styles.label, isFocus && { color: "blue" }]}>
+          MultiSelect label
+        </Text>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <View style={styles.container}>
+      {renderLabel()}
+      <MultiSelect
+        style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={data}
+        search
+        maxHeight={300}
+        labelField="key"
+        valueField="key"
+        placeholder={!isFocus ? "Select item" : "..."}
+        searchPlaceholder="Search..."
+        value={value}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          setValue(item);
+          setIsFocus(false);
+        }}
+        // renderLeftIcon={() => (
+        //   <MaterialIcons name="check" size={24} color="black" />
+        // )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 16,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
